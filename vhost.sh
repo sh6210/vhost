71ebc371e7a4461b8621e59de61ee1985cd61aeb
sohag <shcseilacs@gmail.com>
2021-10-27 14:33:38 +0600

    modified vhost.sh
    Added a readonly variable for storing conf_file_path.

--- vhost.sh
+++ vhost.sh
@@ -1,7 +1,7 @@
 read -p "May i know the virtual host name?* " VHOST
 read -p "May i know the root directory name? " ROOT_DIR
 
-filepath="$HOME/mine/bash/vhost_sample.txt"
+readonly CONF_FILE_PATH="$HOME/mine/bash/vhost_sample.txt"
 
 echo "\n Creating conf file inside /etc/apache2/sites-available"
 sudo touch /etc/apache2/sites-available/$VHOST.conf
@@ -10,7 +10,7 @@ echo "\n Running chown to the conf file"
 sudo chown "$USER":"$USER" /etc/apache2/sites-available/$VHOST.conf
 
 echo "\n Filling with virtual host content"
-head -n 20 $HOME/mine/bash/vhost_sample.txt > /etc/apache2/sites-available/$VHOST.conf
+head -n 20 $CONF_FILE_PATH > /etc/apache2/sites-available/$VHOST.conf
 
 sudo sed -i "s/test/$VHOST/" /etc/apache2/sites-available/$VHOST.conf
 sudo sed -i "s/project_root_dir/$ROOT_DIR/" /etc/apache2/sites-available/$VHOST.conf
@@ -25,7 +25,7 @@ sudo service apache2 restart
 
 if [ -d "/var/www/html/$VHOST" ]
 then
-   echo "\n Found $VHOST directory." 
+   echo "\n Found $VHOST directory."
 else
     echo "\n creating directory $VHOST"
     mkdir /var/www/html/$VHOST
